2688560cd2686afa835398fd4d3c9ac7c25e1b27
davidcalikes <davidcalikes@gmail.com>
2023-01-11 23:37:53 +0000

    feat: Create product & category models and migrate to database

--- products/models.py
+++ products/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Model for product categories
+    """
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """
+    Model for products
+    """
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(
+        max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    in_stock = models.BooleanField(
+        default=False, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(
+        max_length=1024, null=True, blank=True)
+    image = CloudinaryField(
+        "Product Image", default='placeholder')
+
+    def __str__(self):
+        return self
