9d6669b27b8ff96d76022ba4b7b76aadec989bbe
davidcalikes <davidcalikes@gmail.com>
2023-01-12 23:19:12 +0000

    feat: Add products view and template shell. Wire up urls

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def all_products(request):
+    """ A view to return all products to the products page """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
