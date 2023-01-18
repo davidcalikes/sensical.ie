f29ef6acdde4a6318ad93157379f22ba330c8c21
davidcalikes <davidcalikes@gmail.com>
2023-01-18 23:33:06 +0000

    feat: Add sorting template code to products template and update views.py

--- products/views.py
+++ products/views.py
@@ -1,38 +1,60 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 
 def all_products(request):
-    """ A view to return all products to the products page """
+    """ A view that displays all products."""
 
     products = Product.objects.all()
-    query = ""
-    categories = ""
+    query = None
+    categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if sortkey == 'category':
+                sortkey = 'category__name'
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
-    if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(
                     request, "Please type what you are looking for...")
-                return redirect(
-                    reverse('products'))
+                return redirect(reverse('products'))
 
-    queries = Q(name__icontains=query) | Q(description__icontains=query)
-    products = products.filter(queries)
+            queries = Q(
+                name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(
+                queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
