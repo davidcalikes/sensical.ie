7189685b4a820bb9daecddc7881e067a6fae0992
davidcalikes <davidcalikes@gmail.com>
2023-01-23 10:59:03 +0000

    feat: Create and test functionality that adds items to session basket

--- basket/urls.py
+++ basket/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.shopping_basket, name='shopping_basket')
+    path('', views.shopping_basket, name='shopping_basket'),
+    path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def shopping_basket(request):
@@ -6,3 +6,20 @@ def shopping_basket(request):
 
     return render(request, 'basket/shopping_basket.html')
 
+
+def add_to_basket(request, item_id):
+    """ A view to add a quantity of a sensory product to the shopping basket """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+    else:
+        basket[item_id] = quantity
+
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    print(request.session['basket'])
+    return redirect(redirect_url)
