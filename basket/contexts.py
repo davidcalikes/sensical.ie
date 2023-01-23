311a5fc010a967b50190ab9fd78554a28d51a8c9
davidcalikes <davidcalikes@gmail.com>
2023-01-23 11:47:24 +0000

    feat: Add shopping basket items and totalling functionality to basket.context.py and test

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def basket_contents(request):
@@ -7,6 +9,17 @@ def basket_contents(request):
     basket_items = []
     total = 0
     item_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        item_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
