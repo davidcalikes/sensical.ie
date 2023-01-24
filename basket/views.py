0f82eea90a95ae13c4394931e8378e0479f81b17
davidcalikes <davidcalikes@gmail.com>
2023-01-24 22:19:15 +0000

    feat: Add placeholder quantity selecto and update basket inputr for each basket itm

--- basket/views.py
+++ basket/views.py
@@ -13,7 +13,7 @@ def add_to_basket(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
-
+    
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
     else:
