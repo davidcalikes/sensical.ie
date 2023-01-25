7997ca7cbf68bd7c2feec98aff5431ddeda16b10
davidcalikes <davidcalikes@gmail.com>
2023-01-25 00:02:32 +0000

    feat: Add functionality that updates item quantities from the shopping  basket template

--- basket/urls.py
+++ basket/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.shopping_basket, name='shopping_basket'),
     path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
+    path('update/<item_id>/', views.update_basket, name='update_basket'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 
 def shopping_basket(request):
@@ -8,12 +8,12 @@ def shopping_basket(request):
 
 
 def add_to_basket(request, item_id):
-    """ A view to add a quantity of a sensory product to the shopping basket """
+    """ Add a quantity of a sensory product to the shopping basket """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
-    
+
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
     else:
@@ -21,3 +21,18 @@ def add_to_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def update_basket(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop(item_id)
+
+    request.session['basket'] = basket
+    return redirect(reverse('shopping_basket'))
