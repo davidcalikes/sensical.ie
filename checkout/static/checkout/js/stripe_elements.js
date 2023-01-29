3360dfa0f15a06cc84061b6bdcb41e5809d0d60a
davidcalikes <davidcalikes@gmail.com>
2023-01-29 17:50:04 +0000

    feat: Add placeholder loading modal

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -34,7 +34,7 @@ card.addEventListener('change', function (event) {
     if (event.error) {
         let html = `
             <span class="icon" role="alert">
-            <ion-icon name="warning-outline"></ion-icon>
+            <i class="fas fa-times"></i>
             </span>
             <span>${event.error.message}</span>
         `;
@@ -51,6 +51,8 @@ form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -64,6 +66,8 @@ form.addEventListener('submit', function(ev) {
                 </span>
                 <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
             card.update({ 'disabled': false});
             $('#submit-button').attr('disabled', false);
         } else {
