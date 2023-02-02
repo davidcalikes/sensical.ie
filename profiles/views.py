d9521db6bf96d562b20957a8f2d938db23e9edd3
davidcalikes <davidcalikes@gmail.com>
2023-02-02 22:28:45 +0000

    feat: Create add functionlity to add product form/template

--- profiles/views.py
+++ profiles/views.py
@@ -16,8 +16,10 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Invalid form submission!')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
