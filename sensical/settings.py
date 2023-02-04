ceb7e3d68f6ec82f25d6b7a7a923b58dda481750
davidcalikes <davidcalikes@gmail.com>
2023-02-04 15:40:11 +0000

    feat: Add custom data model to wishlist app

--- sensical/settings.py
+++ sensical/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'basket',
     'checkout',
     'profiles',
-    'wishlist'
+    'wishlist',
 
     'crispy_forms',
 ]
--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+from products.models import Product
+
+
+class wishlist(models.Model):
+    userid = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
