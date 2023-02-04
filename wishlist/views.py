24c16223642a7e6681982e7a9f1965d6166237f0
davidcalikes <davidcalikes@gmail.com>
2023-02-04 15:46:50 +0000

    feat: Add wishlist view class and link to template

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import wishlist
+from products.models import Product
+from django.contrib.auth.models import User
+
+
+class wishlistView(TemplateView):
+
+    template_name = 'wishlist.html'
+
+    def post(self, request, pk):
+        product = self.request.POST.get('name')
+        current_user = request.user
+        product_wish = wishlist(userid_id=current_user.id, product_id=product)
+        book.save()
+        return
