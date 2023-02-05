2e05dbcfcfc5bd212f5750412a8e47862b5ab04d
davidcalikes <davidcalikes@gmail.com>
2023-02-05 17:12:30 +0000

    feat: Add template logic to restrict product management to superusers only

--- wishlist/contexts.py
+++ wishlist/contexts.py
@@ -1,10 +1,12 @@
 from django.shortcuts import get_object_or_404
 from products.models import Product
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 
 def get_wishlist_status(request):
-
-    products = Product.objects.filter(users_wishlist=request.user)
+    products = 0
+    if request.user.is_authenticated:
+        products = Product.objects.filter(users_wishlist=request.user)
 
     return {"users_wishlist": products}
