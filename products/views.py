5020ccff3e449bafe8a89cdbdb362416be63eac7
davidcalikes <davidcalikes@gmail.com>
2023-02-07 23:32:53 +0000

    feat: Add layout and custom CSS to wishlist success modal

--- products/views.py
+++ products/views.py
@@ -81,8 +81,7 @@ def add_product(request):
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             product = form.save()
-            messages.success(request, 'Successfully added product!',
-                             extra_tags=['basket_message'])
+            messages.success(request, 'Successfully added product!',)
             return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request,
