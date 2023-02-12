5315c293c778110d73829a9551381d6c04972a74
davidcalikes <davidcalikes@gmail.com>
2023-02-12 22:32:26 +0000

    feat: Add custom data model to testimonials app and makemigrations

--- testimonials/models.py
+++ testimonials/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class clientTestimonial(models.Model):
+    """
+    Model class for the testimonials app
+    """
+
+    name = models.CharField(max_length=256)
+    testimonial = models.TextField(blank=True, null=True)
+    photo_url = models.URLField(max_length=1024, blank=True, null=True)
+    image = CloudinaryField(
+        "Product Image", default='placeholder')
+
+    def __str__(self):
+        return self.name
