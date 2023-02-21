273e311fd7b8057471c43eadebb2e19a72eac291
davidcalikes <davidcalikes@gmail.com>
2023-02-21 17:50:50 +0000

    feat: Add Temlate, create, update and delete views to packages app

--- packages/views.py
+++ packages/views.py
@@ -8,40 +8,40 @@ from .models import Packages
 from .forms import PackageForm
 
 
-class AllTestimonials(TemplateView):
-    """A view that reders all client testimonials"""
+class CurrentPackages(TemplateView):
+    """A view that reders all available hire packages"""
 
-    template_name = "testimonials/testimonials.html"
+    template_name = "packages/packages.html"
 
     def get_context_data(self, **kwargs):
-        testimonials = clientTestimonial.objects.all()
-        all_testimonials_list = []
-        for testimonial in testimonials:
-            all_testimonials_list.append(testimonial)
+        packages = Packages().objects.all()
+        all_packages_list = []
+        for package in packages:
+            all_packages_list.append(package)
 
         context = super().get_context_data(**kwargs)
-        context["testimonials"] = testimonials
-        context["all_testimonials_list"] = all_testimonials_list
+        context["packages"] = packages
+        context["all_packages_list"] = all_packages_list
 
         return context
 
 
-class AddTestimonial(generic.CreateView):
+class AddPackage(generic.CreateView):
     """
-    Superuser can add a testimonial
+    Superuser can add a package
     """
-    model = clientTestimonial
-    form_class = testimonialForm
-    template_name = 'testimonials/testimonial_form.html'
-    success_url = reverse_lazy('testimonials')
-    success_message = "Testimonial added successfully!"
+    model = Packages()
+    form_class = PackageForm
+    template_name = 'packages/packages_form.html'
+    success_url = reverse_lazy('packages')
+    success_message = "Package added successfully!"
 
     def form_valid(self, form):
         """
         Auto applies foreign key and auto generated settings.
         """
         form.instance.created_by = self.request.user
-        return super(AddTestimonial, self).form_valid(form)
+        return super(AddPackage, self).form_valid(form)
 
     def form_invalid(self, form):
         """If the form is invalid, render the invalid form."""
@@ -50,15 +50,15 @@ class AddTestimonial(generic.CreateView):
         return self.render_to_response(self.get_context_data(form=form))
 
 
-class UpdateTestimonial(generic.edit.UpdateView):
+class UpdatePackage(generic.edit.UpdateView):
     """
-    Superuser can update enrolled an existing testimonial
+    Superuser can update enrolled an existing package
     """
-    model = clientTestimonial
-    form_class = testimonialForm
-    template_name = 'testimonials/testimonial_form.html'
-    success_url = reverse_lazy('testimonials')
-    success_message = "Testimonial updated successfully!"
+    model = Packages()
+    form_class = PackageForm()
+    template_name = 'packages/packages_form.html'
+    success_url = reverse_lazy('packages')
+    success_message = "Package updated successfully!"
 
     def form_invalid(self, form):
         """If the form is invalid, render the invalid form."""
@@ -67,16 +67,16 @@ class UpdateTestimonial(generic.edit.UpdateView):
         return self.render_to_response(self.get_context_data(form=form))
 
 
-class DeleteTestimonial(generic.DeleteView):
+class DeletePackage(generic.DeleteView):
     """
-    Superuser can delete an existing testimonial
+    Superuser can delete an existing package
     """
-    model = clientTestimonial
-    form_class = testimonialForm
-    template_name = 'testimonials/testimonial_form.html'
-    success_url = reverse_lazy('testimonials')
-    success_message = "Testimonial deleted successfully!"
+    model = Packages()
+    form_class = PackageForm()
+    template_name = 'packages/packages_form.html'
+    success_url = reverse_lazy('packages')
+    success_message = "Package deleted successfully!"
 
     def delete(self, request, *args, **kwargs):
         messages.success(self.request, self.success_message)
-        return super(DeleteTestimonial, self).delete(request, *args, **kwargs)
+        return super(DeletePackage, self).delete(request, *args, **kwargs)
