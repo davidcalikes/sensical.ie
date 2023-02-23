70ed0e8447d7ca5c85cb75b611d1f309fb952007
davidcalikes <davidcalikes@gmail.com>
2023-02-23 21:31:08 +0000

    feat: Create link to add products for superuser in products template

--- about/urls.py
+++ about/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('about/', views.about, name='about')
+    path('', views.about, name='about')
 ]
--- testimonials/urls.py
+++ testimonials/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import AllTestimonials, AddTestimonial, UpdateTestimonial, DeleteTestimonial
 
 urlpatterns = [
-    path("testimonials/", AllTestimonials.as_view(), name="testimonials"),
+    path("", AllTestimonials.as_view(), name="testimonials"),
     path("add/", AddTestimonial.as_view(), name="add_testimonial"),
     path("update/<int:pk>", UpdateTestimonial.as_view(), name="update_testimonial"),
     path("delete/<int:pk>", DeleteTestimonial.as_view(), name="delete_testimonial"),
