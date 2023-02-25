c4c6100286c55a95979855ecb5c7a16b0c01aaa6
davidcalikes <davidcalikes@gmail.com>
2023-02-25 19:19:07 +0000

    fix: Add explicit height and width to nav images and add robots.txt url for SEO

--- sensical/urls.py
+++ sensical/urls.py
@@ -1,21 +1,6 @@
-"""sensical URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,7 +15,13 @@ urlpatterns = [
     path('testimonials/', include('testimonials.urls')),
     path('hire/', include('hire.urls')),
     path('packages/', include('packages.urls')),
+    path(
+        "robots.txt",
+        TemplateView.as_view(
+            template_name="robots.txt", content_type="text/plain")
+        ),
 ]
+
 
 handler404 = "sensical.views.page_not_found_view"
 handler500 = "sensical.views.error_view"
