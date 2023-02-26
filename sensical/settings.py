eb4154e5903d1f41e612bb8779cc18fa49f774c5
davidcalikes <davidcalikes@gmail.com>
2023-02-26 16:25:33 +0000

    docs: Update Project Management Testing in TESTING.md

--- sensical/settings.py
+++ sensical/settings.py
@@ -20,7 +20,6 @@ if os.path.isfile("env.py"):
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -28,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["sensical-ireland.herokuapp.com", "localhost"]
 
